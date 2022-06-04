62801305b108fa2af92343fae0b1bf59b8a076f8
AmeyaSantoshGidh <98058342+AmeyaSantoshGidh@users.noreply.github.com>
2022-06-04 02:41:26 -0400

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,11 @@
 console.log("This is the java script file.")
+
+function myFunction() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
